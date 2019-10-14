90ab8ab4ed2d6ecc2d4577337ee4066af7282c05
jedw <jedwards8@uclan.ac.uk>
2019-10-14 18:27:04 +0100

    Add files via upload

--- AJAX2/insert.php
+++ AJAX2/insert.php
@@ -1,23 +1,21 @@
 <?php
 if (isset($_POST['uname'])) { //Check if form data has actually been posted
-  $firstname = $_POST['fname']; //Initialize variables from POST data
-  $surname = $_POST['sname'];
-  $username = $_POST['uname'];
-  $password = $_POST['pword'];
-  $email = $_POST['email'];
-  
-  echo"testing";
+  $fn = $_POST['fname']; //Initialize variables from POST data
+  $sn = $_POST['sname'];
+  $un = $_POST['uname'];
+  $pw = $_POST['pword'];
+  $em = $_POST['email'];
 
   include 'connect.php';
   
   $stmt = $mysqli->prepare("INSERT into ajaxcheckuser (firstname, surname, username, password, email) VALUES (?,?,?,?,?)");
-  $stmt->bind_param('sssss', $firstname, $surname, $username, $password, $email);
+  $stmt->bind_param('sssss', $fn, $sn, $un, $pw, $em);
   $success = $stmt->execute(); 
 
   if ($success) { 
     //If executing the query returns TRUE then it has inserted correctly
     echo "Form Submitted successfully";
-    echo "<br/><a href=\"register.php\">Back</a>";
+    echo "<br/><a href=\"index.php\">Back</a>";
   }
     $stmt->close();
 	$mysqli->close();
