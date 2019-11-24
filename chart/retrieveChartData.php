974577892b5d8ea7a136011127182ffe83ac172a
jedw <jedwards8@uclan.ac.uk>
2019-11-24 14:48:51 +0000

    sun

---
+++ chart/retrieveChartData.php
@@ -0,0 +1,27 @@
+<?php
+	$servername = "localhost";
+	$username = "student";
+	$password = "website";
+	$dbasename = "mydatabase";
+	// Create connection
+	$mysqli = new mysqli($servername, $username, $password, $dbasename);
+	// Check connection
+	if ($mysqli->connect_errno) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
+	
+	$query = $mysqli->query("SELECT * FROM chart");
+	
+	$chart = array();
+
+	while($row = $query->fetch_array()){
+		array_push($chart, $row);
+	}
+	
+	$mysqli->close();
+
+	header("Content-type: application/json");
+	echo json_encode($chart);
+	die();
+?>
