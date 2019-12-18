baf231283945ad677bd1f7b765e5c9f4336176ea
jedw <jedwards8@uclan.ac.uk>
2019-12-18 14:34:31 +0000

    laravel

--- laravel101/resources/views/newuser.blade.php
+++ laravel101/resources/views/newuser.blade.php
@@ -90,14 +90,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="users">Users</a>
+                    <a href="users/new">New User</a>
+                 
                 </div>
                 <div>
                     <br><br>
--- laravel101/resources/views/users.blade.php
+++ laravel101/resources/views/users.blade.php
@@ -46,6 +46,7 @@
 
             .content table{
                 text-align: left;
+                color: #000;
             }
 
             .title {
@@ -90,14 +91,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="users">Users</a>
+                    <a href="users/new">New User</a>
+                 
                 </div>
                 <div>
                     <br><br>
