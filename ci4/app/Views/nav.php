c9e8e5405fa699ff8532a3255e8d2b1668a5210f
jedw <you@example.com>
2020-06-16 12:05:27 -0700

    sqlexport

--- ci4/app/Views/nav.php
+++ ci4/app/Views/nav.php
@@ -1,7 +1,7 @@
 <nav>
     <ul class="nav nav-tabs">
         <li>
-            <a class="nav-link active" href="<?php echo site_url();?>">Home</a>
+            <a class="nav-link" href="<?php echo site_url();?>">Home</a>
         </li>
         <li>
             <a class="nav-link" href="<?php echo site_url('home/records');?>">Records</a>
@@ -10,7 +10,7 @@
             <a class="nav-link" href="<?php echo site_url('home/create');?>">Add</a>
         </li>
         <li>
-            <a class="nav-link" href="<?php echo site_url('home/page4');?>">Link</a>
+            <a class="nav-link" href="<?php echo site_url('home');?>">Link</a>
         </li>
     </ul>
 </nav>
