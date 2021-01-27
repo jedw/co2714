a3fc59391591d152ebdec5a67d2d9ed59d2cfbab
Jonathan <jedwards8@uclan.ac.uk>
2021-01-27 19:22:44 +0000

    ci4ajax

--- ci4ajax/app/Controllers/Home.php
+++ ci4ajax/app/Controllers/Home.php
@@ -1,11 +1,16 @@
 <?php namespace App\Controllers;
 
 use App\Models\AjaxModel;
+use App\Models\CheckUserModel;
 
 class Home extends BaseController
 {
 	public function index(){
 		return view('home');
+	}
+
+	public function register(){
+		return view('register');
 	}
 
 	public function things()
@@ -20,6 +25,23 @@ class Home extends BaseController
 		$model = new AjaxModel();
 		$things['thing'] = $this->request->getPost('thing');
 		$model->addNewThing($things);
+	}
+
+	public function checkUser()
+	{
+		$user = $this->request->getPost('uname');
+		$model = new CheckUserModel();
+		$check = $model->getUserWhere($user);
+
+		if ($check)
+		{
+			$jsonReply['availability'] = false; 
+        	return $this->response->setJSON($jsonReply);
+		} else
+		{
+			$jsonReply['availability'] = true; 
+        	return $this->response->setJSON($jsonReply);
+        }
 	}
 	//--------------------------------------------------------------------
 }
---
+++ ci4ajax/app/Models/CheckUserModel.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Database\ConnectionInterface;
+use CodeIgniter\Model;
+
+class CheckUserModel extends Model{
+
+    protected $table = "ajaxcheckuser";
+    protected $primaryKey = 'ID';
+    protected $allowedFields = ['ID', 'firstname', 'surname', 'email', 'username', 'password', 'email'];
+    protected $returnType = 'array';
+
+    public function getUserWhere($username)
+    {
+        $data = $this->where('username', $username)->first();
+        if ($data){
+            return TRUE;
+        }
+        else{
+            return FALSE;
+        }
+    }
+    
+}
